7a47847c9c27ad7b50c7d5f7057f684915b42115
Rittu17 <rittuphilipgeorge28@gmail.com>
2025-11-25 17:20:07 +0530

    Add files via upload
    Week1-Day1

---
+++ WEEK1/DAY1/ASSIGNMENT1-PRIMITIVEDATATYPES.js
@@ -0,0 +1,31 @@
+//string  a) firstName
+
+let  firstName= "Meena"
+console.log(firstName)
+console.log(typeof firstName)
+
+//string b) companyName
+
+let  companyName= "QASolutions"
+console.log(companyName)
+console.log(typeof companyName)
+
+
+//number  c) mobileNumber
+
+let  mobileNumber= 123456789
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+//boolean d) isAutomation
+
+let  isAutomation= true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+// e) hasPlaywright (do not assign)
+let  hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
+
+
